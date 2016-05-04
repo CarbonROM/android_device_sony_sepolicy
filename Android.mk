6bae73ad114c040cb09812c2e9c898db4c04453e
Christian Oder <myself5@carbonrom.org>
2016-05-04 14:43:41 +0200

    move sepolicy repo to sony dir

--- Android.mk
+++ Android.mk
@@ -2,8 +2,8 @@
 # we don't want to force a reset of BOARD_SEPOLICY_DIRS
 #
 # If you want to use these policies, add a
-# 
-# include device/qcom/sepolicy/sepolicy.mk
+#
+# include device/sony/sepolicy/sepolicy.mk
 #
 # to your device's BoardConfig. It is highly recommended that in case
 # you have your own BOARD_SEPOLICY_DIRS and BOARD_SEPOLICY_UNION declarations,
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Repository that hosts SELinux common policies for QCOM Hardware
 
 If you want to use these policies, add a
 
-include device/qcom/sepolicy/sepolicy.mk
+include device/sony/sepolicy/sepolicy.mk
 
 to your device's BoardConfig. It is highly recommended that in case
 you have your own BOARD_SEPOLICY_DIRS and BOARD_SEPOLICY_UNION declarations,
